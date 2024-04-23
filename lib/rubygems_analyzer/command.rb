01c0cbf09de56d2de54fed153eca11b73fe3a825
aiandrox <aiandrox7@gmail.com>
2024-04-23 23:41:43 +0900

    `bin/rubygems_analyzer "{\"hoge\": 50000000000000, \"foo\": 10}"`でグラフを描画するようにした

--- lib/rubygems_analyzer/command.rb
+++ lib/rubygems_analyzer/command.rb
@@ -5,5 +5,15 @@ module RubygemsAnalyzer
     def self.run(argument)
       puts "Hello, \e[35m\"#{argument}\"\e[0m"
     end
+
+    def self.draw_bar_chart(data)
+      max_key_length = data.keys.max_by(&:length).length
+      scale_factor = data.values.max / 50 # 50 is the max length of the graph
+
+      data.each do |key, value|
+        graph_length = value / scale_factor
+        puts "#{key.ljust(max_key_length)} ┤\e[33m#{'■' * graph_length}\e[0m #{value}"
+      end
+    end
   end
 end
