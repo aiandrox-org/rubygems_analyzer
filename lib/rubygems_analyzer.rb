e91349bceb34659bdc3d5d4987fe942c6b713772
aiandrox <aiandrox7@gmail.com>
2024-04-23 23:15:10 +0900

    $ bin/rubygems_analyzer argument で引数を出力する

--- lib/rubygems_analyzer.rb
+++ lib/rubygems_analyzer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'rubygems_analyzer/version'
+require_relative 'rubygems_analyzer/command'
 
 module RubygemsAnalyzer
   class Error < StandardError; end
---
+++ lib/rubygems_analyzer/command.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+module RubygemsAnalyzer
+  class Command
+    def self.run(argument)
+      puts "Hello, #{argument}!"
+    end
+  end
+end
