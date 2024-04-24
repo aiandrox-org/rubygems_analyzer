a61b29f5ccaad0020457b15696a6b565001fa0e7
aiandrox <aiandrox7@gmail.com>
2024-04-24 18:43:30 +0900

    リファクタリング

--- lib/rubygems_analyzer/command.rb
+++ lib/rubygems_analyzer/command.rb
@@ -6,17 +6,18 @@ require_relative 'ruby_gem'
 module RubygemsAnalyzer
   class Command
     def self.run(gem_name)
-      data = File.open(File.expand_path('lib/rubygems_analyzer/rubygems_stats.yml'), 'r') do |f|
-        YAML.load(f)
-      end['gem_stats']
-      ruby_gems = data.map { |gem| RubyGem.new(name: gem['name'], downloads: gem['downloads']) }
-      ordered_data = ruby_gems.sort_by(&:downloads).reverse
+      new(gem_name).run
+    end
 
-      target_gem_stats = ruby_gems.find { _1.name == gem_name }
-      return puts 'No such gem' unless target_gem_stats
+    def initialize(gem_name)
+      @gem_name = gem_name
+    end
 
-      max_name_length = ruby_gems.map { _1.name.length }.max
-      scale_factor = ruby_gems.map(&:downloads).max / 50 # is the max length of the graph
+    def run
+      return puts 'No such gem' unless target_gem
+
+      max_name_length = ordered_data.map { _1.name.length }.max
+      scale_factor = ordered_data.map(&:downloads).max / 50 # is the max length of the graph
 
       ordered_data.each do |gem|
         graph_length = gem.downloads / scale_factor
@@ -24,6 +25,24 @@ module RubygemsAnalyzer
         graph_bar = "#{color}#{'■' * graph_length}\e[0m"
         puts "#{gem.name.ljust(max_name_length)} ┤#{graph_bar} #{gem.downloads}"
       end
+    end
+
+    private
+
+    attr_reader :gem_name
+
+    def ordered_data
+      @ordered_data ||= begin
+        data = File.open(File.expand_path('lib/rubygems_analyzer/rubygems_stats.yml'), 'r') do |f|
+          YAML.load(f)
+        end['gem_stats']
+        ruby_gems = data.map { |gem| RubyGem.new(name: gem['name'], downloads: gem['downloads']) }
+        ruby_gems.sort_by(&:downloads).reverse
+      end
+    end
+
+    def target_gem
+      @target_gem ||= ordered_data.find { _1.name == gem_name }
     end
   end
 end
