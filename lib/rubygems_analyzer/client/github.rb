bf8c3fa4131fec860e624cfcbf61f7803fe7977e
kenken <kengoviolinphoto@gmail.com>
2024-04-25 18:26:14 +0900

    Githubのスター数を取得できるようにした

---
+++ lib/rubygems_analyzer/client/github.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'octkit'
+require 'dotenv/load'
+
+module RubygemsAnalyzer
+  module Client
+    class Github
+      attr_reader :client
+
+      def initialize
+        @client = Octokit::Client.new(access_token: ENV.fetch('GITHUB_ACCESS_TOKEN', nil))
+      end
+
+      def get_star(repo_name:)
+        client.repository(repo_name)[:stargazers_count]
+      end
+
+      def repo
+        @repo ||= client.repository(repo_name)
+    end
+  end
+end
